4a2fa2530be8aac9bfdd9db793bb35efa2c744c5
nguyendai123 <nguyenvandai1232001@gmail.com>
2023-10-11 09:15:34 +0700

    update config security

--- src/main/java/com/thanhson/bookhup/config/SpringSecurityConfig.java
+++ src/main/java/com/thanhson/bookhup/config/SpringSecurityConfig.java
@@ -1,3 +1,87 @@
+//package com.thanhson.bookhup.config;
+//
+//import com.thanhson.bookhup.jwts.AuthEntryPointJwt;
+//import com.thanhson.bookhup.jwts.AuthTokenFilter;
+//import lombok.AllArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import java.util.Arrays;
+//
+//@Configuration
+//@AllArgsConstructor
+//@EnableWebSecurity
+//public class SpringSecurityConfig  {
+//    private UserDetailsService userDetailsService;
+//
+//    private AuthTokenFilter authTokenFilter;
+//
+//
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//
+//        authProvider.setUserDetailsService(userDetailsService);
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//
+//        return authProvider;
+//    }
+//
+//    @Bean
+//    public static PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        http.cors().and().csrf().disable()
+//                .authorizeHttpRequests((authorize) -> {
+//                    authorize.requestMatchers("/api/auth/**").permitAll();
+//                    authorize.requestMatchers("/api/books/**").permitAll();
+//                    authorize.requestMatchers("/api/genres/**").permitAll();
+//                    authorize.requestMatchers("/api/posts/**").permitAll();
+//                    authorize.requestMatchers("/api/comment/**").permitAll();
+//                    authorize.requestMatchers("/api/**").permitAll();
+//
+//                    authorize.anyRequest().authenticated();
+//                });
+//
+//        http.authenticationProvider(authenticationProvider());
+//
+//        http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+//        return configuration.getAuthenticationManager();
+//    }
+//
+//    @Bean
+//    CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("*"));
+//        configuration.setAllowedMethods(Arrays.asList("*"));
+//        configuration.setAllowedHeaders(Arrays.asList("*"));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+//}
 package com.thanhson.bookhup.config;
 
 import com.thanhson.bookhup.jwts.AuthEntryPointJwt;
@@ -7,8 +91,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,9 +108,10 @@ import java.util.Arrays;
 
 @Configuration
 @AllArgsConstructor
-public class SpringSecurityConfig {
-    private UserDetailsService userDetailsService;
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private UserDetailsService userDetailsService;
     private AuthTokenFilter authTokenFilter;
 
     @Bean
@@ -42,17 +129,16 @@ public class SpringSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
-                .authorizeHttpRequests((authorize) -> {
-                    authorize.requestMatchers("/api/auth/**").permitAll();
-                    authorize.requestMatchers("/api/books/**").permitAll();
-                    authorize.requestMatchers("/api/genres/**").permitAll();
-                    authorize.requestMatchers("/api/posts/**").permitAll();
-                    authorize.requestMatchers("/api/comment/**").permitAll();
-                    authorize.requestMatchers("/api/**").permitAll();
+                .authorizeRequests((authorize) -> {
+                    authorize.antMatchers("/api/auth/**").permitAll();
+                    authorize.antMatchers("/api/books/**").permitAll();
+                    authorize.antMatchers("/api/genres/**").permitAll();
+                    authorize.antMatchers("/api/posts/**").permitAll();
+                    authorize.antMatchers("/api/comment/**").permitAll();
+                    authorize.antMatchers("/api/**").permitAll();
 
                     authorize.anyRequest().authenticated();
                 });
@@ -60,13 +146,12 @@ public class SpringSecurityConfig {
         http.authenticationProvider(authenticationProvider());
 
         http.addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
